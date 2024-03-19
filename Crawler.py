14703fd9fe1911aabf3c116859f7b085d5bf017b
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-03-19 14:03:50 +0530

    removing stopwords code is added

--- Crawler.py
+++ Crawler.py
@@ -5,13 +5,17 @@ import re
 import os
 
 class RankingAlgorithm:
-    def __init__(self, index):
+    def __init__(self, index, stop_words):
         self.index = index
+        self.stop_words = stop_words
 
     def rank_pages(self, query):
-        query_words = query.lower().split()
+        # Remove stopwords from the query
+        query_words = [word for word in query.lower().split() if word not in self.stop_words]
+        
         relevant_pages = set()
         for query_word in query_words:
             relevant_pages.update(self.index.get(query_word, []))
-        scores = {page: sum(page.count(word) for word in query_words) for page in relevant_pages}
+        
+        scores = {page: sum(page.lower().count(word) for word in query_words) for page in relevant_pages}
         return scores
