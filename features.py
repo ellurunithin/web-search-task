8c0da99e092d7199b4fae901fd6a5e0a569354ef
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-03-19 12:00:11 +0530

    comments are added

--- features.py
+++ features.py
@@ -4,14 +4,15 @@ from collections import defaultdict
 import re
 import os
 
+
 class Indexer:
     def __init__(self):
-        self.index = defaultdict(list)
+        self.index = defaultdict(list)  # Creating a defaultdict to store index data
 
     def index_page(self, url, text):
-        words = re.findall(r'\b\w+\b', text.lower())
+        words = re.findall(r'\b\w+\b', text.lower())  # Extracting words from text using regex
         stop_words = {'and', 'the', 'in', 'of', 'a'}  # Example stop words
-        words = [word for word in words if word not in stop_words]
+        words = [word for word in words if word not in stop_words]  # Removing stop words
 
-        for word in words:
-            self.index[word].append(url)
+        for word in words:  # Looping through each word
+            self.index[word].append(url)  # Adding URL to the index dictionary under corresponding word
