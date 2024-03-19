4c1143477274c9e5aa7e8e41620fe50aa2413a33
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-03-19 14:59:27 +0530

    rank_pages is modified

--- Crawler.py
+++ Crawler.py
@@ -28,7 +28,7 @@ class Indexer:
 
     def index_page(self, url, text):
         words = re.findall(r'\b\w+\b', text.lower())  # Extracting words from text using regex
-        words = [word for word in words if word not in stop_words]  # Removing stopwords
+        words = [word for word in words if word not in self.stop_words]  # Removing stopwords
 
         for word in words:  # Looping through each word
             self.index[word].append(url)  # Adding URL to the index dictionary under corresponding word
