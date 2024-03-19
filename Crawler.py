ff39ee621b65d02a19103e90830607b1a237009e
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-03-19 12:41:28 +0530

    feature_Ranking implemented

--- Crawler.py
+++ Crawler.py
@@ -3,19 +3,15 @@ from bs4 import BeautifulSoup
 from collections import defaultdict
 import re
 import os
-import nltk  # Importing nltk for natural language processing
-from nltk.corpus import stopwords  # Importing stopwords from NLTK
 
-nltk.download('stopwords')  # Downloading NLTK stopwords
-stop_words = set(stopwords.words('english'))  # Creating set of English stopwords
+class RankingAlgorithm:
+    def __init__(self, index):
+        self.index = index
 
-class Indexer:
-    def __init__(self):
-        self.index = defaultdict(list)  # Creating a defaultdict to store index data
-
-    def index_page(self, url, text):
-        words = re.findall(r'\b\w+\b', text.lower())  # Extracting words from text using regex
-        words = [word for word in words if word not in stop_words]  # Removing stopwords
-
-        for word in words:  # Looping through each word
-            self.index[word].append(url)  # Adding URL to the index dictionary under corresponding word
+    def rank_pages(self, query):
+        query_words = query.lower().split()
+        relevant_pages = set()
+        for query_word in query_words:
+            relevant_pages.update(self.index.get(query_word, []))
+        scores = {page: sum(page.count(word) for word in query_words) for page in relevant_pages}
+        return scores
